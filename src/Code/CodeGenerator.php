149de5ab70976b178e0e11fa66b05ea7e4a24537
Red Forks <redforks@gmail.com>
2020-03-28 17:05:12 +0800

    CodeGenerator->nextPrefixedCode()

---
+++ src/Code/CodeGenerator.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types=1);
+
+namespace Bungle\Framework\Code;
+
+use Doctrine\ODM\MongoDB\DocumentManager;
+use MongoDB\Operation\FindOneAndUpdate;
+use RangeException;
+
+/**
+ * Service generate entity code, or helps to generate entity code.
+ */
+class CodeGenerator
+{
+    public const ID_COLLECTION = 'bungle.code_gen';
+
+    /**
+     * @var DocumentManager
+     */
+    private DocumentManager $dm;
+
+    public function __construct(DocumentManager $dm)
+    {
+        $this->dm = $dm;
+    }
+
+    /**
+     * Return auto-inced prefixed code, such as:
+     *
+     *     nextPrefixedCode('foo', 3);
+     *
+     * will return 'foo001' for the first time, then 'foo002' next time.
+     *
+     * Raise an Exception if current code is 'foo999'.
+     *
+     */
+    public function nextPrefixedCode(string $prefix, int $nchar): string
+    {
+        $db = $this->dm->getConfiguration()->getDefaultDB();
+        $coll = $this->dm->getClient()->selectCollection($db, self::ID_COLLECTION);
+        $query   = ['_id' => $prefix, 'current_id' => ['$exists' => true]];
+        $update  = ['$inc' => ['current_id' => 1]];
+        $options = ['upsert' => false, 'returnDocument' => FindOneAndUpdate::RETURN_DOCUMENT_AFTER];
+        $result  = $coll->findOneAndUpdate($query, $update, $options);
+
+        /*
+         * Updated nothing - counter doesn't exist, creating new counter.
+         * Not bothering with {$exists: false} in the criteria as that won't avoid
+         * an exception during a possible race condition.
+         */
+        if ($result === null) {
+            $query   = ['_id' => $prefix];
+            $update  = ['$inc' => ['current_id' => 1]];
+            $options = ['upsert' => true, 'returnDocument' => FindOneAndUpdate::RETURN_DOCUMENT_AFTER];
+            $coll->findOneAndUpdate($query, $update, $options);
+
+            $result = ['current_id' => 1];
+        }
+
+        $r = $prefix.sprintf("%0${nchar}d", $result['current_id']);
+        if (strlen($r) > strlen($prefix) + $nchar) {
+            throw new RangeException("Max code reached: $r, n: $nchar");
+        }
+        return $r;
+    }
+}
---
+++ tests/Code/CodeGeneratorTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Bungle\Framework\Tests\Code;
+
+use Bungle\Framework\Code\CodeGenerator;
+use Doctrine\ODM\MongoDB\Configuration;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Document;
+use MongoDB\Client;
+use PHP_CodeSniffer\Reports\Code;
+use PHPUnit\Framework\TestCase;
+use RangeException;
+
+class CodeGeneratorTest extends TestCase
+{
+    const TEST_DB = 'test';
+
+    private DocumentManager $dm;
+
+    protected function setUp(): void
+    {
+        $client = new Client(
+            'mongodb://localhost:27017', [],
+            ['typeMap' => ['root' => 'array', 'document' => 'array']]
+        );
+        $config = new Configuration();
+        $config->setDefaultDB(self::TEST_DB);
+        $config->setHydratorDir('/tmp/mongo_db_test/hydrator');
+        $config->setHydratorNamespace('Hydrators');
+
+        $this->dm = DocumentManager::create($client, $config);
+        $coll = $client->selectCollection(self::TEST_DB, CodeGenerator::ID_COLLECTION);
+        $coll->drop();
+    }
+
+    public function testNextPrefixedCode()
+    {
+        $gen = new CodeGenerator($this->dm);
+        self::assertEquals('foo001', $gen->nextPrefixedCode('foo', 3));
+        self::assertEquals('bar1', $gen->nextPrefixedCode('bar', 1));
+        self::assertEquals('foo002', $gen->nextPrefixedCode('foo', 3));
+        self::assertEquals('foo003', $gen->nextPrefixedCode('foo', 3));
+    }
+
+    public function testNextPrefixedCodeOutOfRange(): void
+    {
+        $gen = new CodeGenerator($this->dm);
+        for ($i = 0; $i < 9 ; $i++) {
+            $gen->nextPrefixedCode('foo', 1);
+        }
+
+        $this->expectException(RangeException::class);
+        $gen->nextPrefixedCode('foo', 1);
+    }
+}
