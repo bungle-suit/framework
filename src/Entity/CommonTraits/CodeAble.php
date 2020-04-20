a4b6afcfda534eff5cfc6fcafe8b268ca5c73edd
Red Forks <redforks@gmail.com>
2020-04-20 20:41:40 +0800

    CodeAble field to be unique

--- src/Entity/CommonTraits/CodeAble.php
+++ src/Entity/CommonTraits/CodeAble.php
@@ -3,15 +3,25 @@ declare(strict_types=1);
 
 namespace Bungle\Framework\Entity\CommonTraits;
 
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
 trait CodeAble
 {
+    /**
+     * Is undef for a new document.
+     *
+     * @ODM\Field(type="string")
+     * @ODM\Index(unique=true)
+     */
     protected string $code = '';
 
-    public function getCode(): string {
+    public function getCode(): string
+    {
         return $this->code;
     }
 
-    public function setCode(string $code): void {
+    public function setCode(string $code): void
+    {
         $this->code = $code;
     }
 }
