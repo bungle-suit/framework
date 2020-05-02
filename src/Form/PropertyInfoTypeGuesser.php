968d6b95c17e59fee8ee81df762c09beb1457caf
Red Forks <redforks@gmail.com>
2020-05-02 21:09:57 +0800

    Implement PropertyInfoTypeGuesser

---
+++ src/Form/PropertyInfoTypeGuesser.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+namespace Bungle\Framework\Form;
+
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormTypeGuesserInterface;
+use Symfony\Component\Form\Guess\Guess;
+use Symfony\Component\Form\Guess\TypeGuess;
+use Symfony\Component\Form\Guess\ValueGuess;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractorInterface;
+use Symfony\Component\PropertyInfo\Type;
+
+class PropertyInfoTypeGuesser implements FormTypeGuesserInterface
+{
+    private PropertyInfoExtractorInterface $propertyInfoExtractor;
+
+    public function __construct(PropertyInfoExtractorInterface $propertyInfoExtractor)
+    {
+        $this->propertyInfoExtractor = $propertyInfoExtractor;
+    }
+
+    public function guessType(string $class, string $property): ?TypeGuess
+    {
+        $types = $this->propertyInfoExtractor->getTypes($class, $property);
+        if (0 === count($types)) {
+            return null;
+        }
+
+        if ($types[0]->isCollection()) {
+            return null;
+        }
+
+        switch ($types[0]->getBuiltinType()) {
+            case Type::BUILTIN_TYPE_STRING:
+                return new TypeGuess(TextType::class, [], Guess::LOW_CONFIDENCE);
+            case Type::BUILTIN_TYPE_INT:
+                return new TypeGuess(IntegerType::class, [], Guess::LOW_CONFIDENCE);
+            case Type::BUILTIN_TYPE_FLOAT:
+                return new TypeGuess(NumberType::class, [], Guess::LOW_CONFIDENCE);
+            case Type::BUILTIN_TYPE_BOOL:
+                return new TypeGuess(CheckboxType::class, [], Guess::LOW_CONFIDENCE);
+        }
+
+        return null;
+    }
+
+    public function guessRequired(string $class, string $property): ?ValueGuess
+    {
+        return null;
+    }
+
+    public function guessMaxLength(string $class, string $property): ?ValueGuess
+    {
+        return null;
+    }
+
+    public function guessPattern(string $class, string $property): ?ValueGuess
+    {
+        return null;
+    }
+}
---
+++ tests/Form/PropertyInfoTypeGuesserTest.php
@@ -0,0 +1,75 @@
+<?php
+/** @noinspection PhpParamsInspection */
+declare(strict_types=1);
+
+namespace Bungle\Framework\Tests\Form;
+
+use Bungle\Framework\Form\PropertyInfoTypeGuesser;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Guess\Guess;
+use Symfony\Component\Form\Guess\TypeGuess;
+use Symfony\Component\PropertyInfo\PropertyInfoExtractorInterface;
+use Symfony\Component\PropertyInfo\Type;
+
+class PropertyInfoTypeGuesserTest extends MockeryTestCase
+{
+    /** @var Mockery\MockInterface|Mockery\LegacyMockInterface|PropertyInfoExtractorInterface */
+    private $propertyInfoExtractor;
+    private PropertyInfoTypeGuesser $guesser;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->propertyInfoExtractor = Mockery::mock(PropertyInfoExtractorInterface::class);
+        $this->guesser = new PropertyInfoTypeGuesser($this->propertyInfoExtractor);
+    }
+
+    public function testGuessValue(): void
+    {
+        self::assertNull($this->guesser->guessRequired(self::class, 'foo'));
+        self::assertNull($this->guesser->guessMaxLength(self::class, 'foo'));
+        self::assertNull($this->guesser->guessPattern(self::class, 'foo'));
+    }
+
+    public function testIgnoreGuessTypeIfFailedExtractPropertyType(): void
+    {
+        $this->propertyInfoExtractor->expects('getTypes')->with(self::class, 'foo')
+            ->andReturn([]);
+        self::assertNull($this->guesser->guessType(self::class, 'foo'));
+    }
+
+    /**
+     * @dataProvider guessTypeProvider
+     */
+    public function testGuessType(Type $propType, string $formType, array $formOptions): void
+    {
+        $this->propertyInfoExtractor->expects('getTypes')->with(self::class, 'foo')
+            ->andReturn([$propType]);
+
+        if ($formType === "null") {
+            self::assertNull($this->guesser->guessType(self::class, 'foo'));
+        } else {
+            $exp = new TypeGuess($formType, $formOptions, Guess::LOW_CONFIDENCE);
+            self::assertEquals($exp, $this->guesser->guessType(self::class, 'foo'));
+        }
+    }
+
+    public function guessTypeProvider(): array
+    {
+        $collType = new Type(Type::BUILTIN_TYPE_INT, false, null, true);
+        return [
+            [new Type(Type::BUILTIN_TYPE_NULL), 'null', []],
+            [new Type(Type::BUILTIN_TYPE_INT), IntegerType::class, []],
+            [new Type(Type::BUILTIN_TYPE_FLOAT), NumberType::class, []],
+            [new Type(Type::BUILTIN_TYPE_STRING), TextType::class, []],
+            [new Type(Type::BUILTIN_TYPE_BOOL), CheckboxType::class, []],
+            [$collType, 'null', []],
+        ];
+    }
+}
