75def7cbecd7d534d4048c5572e54c7c1ce55d57
Red Forks <redforks@gmail.com>
2020-06-28 20:52:21 +0800

    Remove assert php option tweak

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,10 +5,3 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 require __DIR__.'/../../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader('class_exists');
-
-ini_set('assert.exception', '1');
-ini_set('assert.active', '1');
-
-if ('1' != ini_get('zend.assertions')) {
-    trigger_error('Set zend.assertions=1 in your php.ini');
-}
