1a2a8898dd55b1cbb63cb76a75a5098a978c18a3
Red Forks <redforks@gmail.com>
2020-09-15 09:34:01 +0800

    Revert "Check not call Query::query() twice"
    This reverts commit 0b758088

--- tests/Inquiry/QueryTest.php
+++ tests/Inquiry/QueryTest.php
@@ -12,7 +12,6 @@ use Bungle\Framework\Inquiry\QueryStepInterface;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
-use LogicException;
 use Mockery;
 use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Symfony\Component\PropertyInfo\Type;
@@ -29,7 +28,7 @@ class QueryTest extends MockeryTestCase
         $this->em = Mockery::mock(EntityManagerInterface::class);
     }
 
-    public function testQuery(): Query
+    public function testQuery(): void
     {
         $qb = Mockery::mock(QueryBuilder::class);
         $this->em->expects('createQueryBuilder')->andReturn($qb);
@@ -55,36 +54,19 @@ class QueryTest extends MockeryTestCase
         );
         $step2->expects('__invoke')->with(Mockery::type(Builder::class));
         $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
-        $q = new Query(
-            $this->em,
-            [
-                $step1,
-                $step2,
-                function (Builder $builder) use ($col1) {
-                    $builder->addColumn($col1, 'foo');
-                },
-            ]
-        );
+        $q = new Query($this->em, [
+            $step1,
+            $step2,
+            function (Builder $builder) use ($col1) {
+                $builder->addColumn($col1, 'foo');
+            },
+        ]);
 
         self::assertEquals([['line1'], ['line2'], ['line3']], iterator_to_array($q->query($params), false));
         self::assertEquals(['foo' => $col1], $q->getColumns());
-
-        return $q;
     }
 
-    /**
-     * @depends testQuery
-     */
-    public function testCanNotQueryTwice(Query $q): void
-    {
-        $this->expectException(LogicException::class);
-        $this->expectExceptionMessage('Can only query once, recreate Query object if needed');
-
-        $params = new QueryParams(0, []);
-        iterator_to_array($q->query($params));
-    }
-
-    public function testPagedQuery(): Query
+    public function testPagedQuery(): void
     {
         $qb1 = Mockery::mock(QueryBuilder::class);
         $qb2 = Mockery::mock(QueryBuilder::class);
@@ -101,36 +83,19 @@ class QueryTest extends MockeryTestCase
         $isBuildForCounts = [];
         $params = new QueryParams(0, []);
         $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
-        $q = new Query(
-            $this->em,
-            [
-                function (Builder $builder) use ($col1, &$isBuildForCounts) {
-                    $isBuildForCounts[] = $builder->isBuildForCount();
-                    if (!$builder->isBuildForCount()) {
-                        $builder->addColumn($col1, 'foo');
-                    }
-                },
-            ]
-        );
+        $q = new Query($this->em, [
+            function (Builder $builder) use ($col1, &$isBuildForCounts) {
+                $isBuildForCounts[] = $builder->isBuildForCount();
+                if (!$builder->isBuildForCount()) {
+                    $builder->addColumn($col1, 'foo');
+                }
+            },
+        ]);
 
         $pagedData = $q->pagedQuery($params);
         self::assertEquals(2, $pagedData->getCount());
         self::assertEquals([['line1'], ['line2']], $pagedData->getData());
         self::assertEquals([false, true], $isBuildForCounts);
         self::assertEquals(['foo' => $col1], $q->getColumns());
-
-        return $q;
-    }
-
-    /**
-     * @depends testPagedQuery
-     */
-    public function testCanNotPagedQueryTwice(Query $q): void
-    {
-        $this->expectExceptionMessage('Can only query once, recreate Query object if needed');
-        $this->expectException(LogicException::class);
-
-        $params = new QueryParams(0, []);
-        $q->pagedQuery($params);
     }
 }
