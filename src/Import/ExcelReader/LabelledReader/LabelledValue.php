b9d44da3db7ee4c7599643a965c538f893153a13
Red Forks <redforks@gmail.com>
2020-11-25 13:43:38 +0800

    Revert "Extract AbstractLabelledValue"
    This reverts commit 3f1249edf0ee0fb8cd51a5e3554bdbe5e1e5a879.

--- src/Import/ExcelReader/LabelledReader/LabelledValue.php
+++ src/Import/ExcelReader/LabelledReader/LabelledValue.php
@@ -3,19 +3,25 @@ declare(strict_types=1);
 
 namespace Bungle\Framework\Import\ExcelReader\LabelledReader;
 
+use Bungle\Framework\FP;
+
 /**
  * @phpstan-template T of object
- * @implements AbstractLabelledValue<T>
+ * @implements LabelledValueInterface<T>
  */
-class LabelledValue extends AbstractLabelledValue
+class LabelledValue implements LabelledValueInterface
 {
+    private string $path;
     private string $label;
+
+    /** @var callable(mixed, Context<T>): mixed; */
+    private $converter;
 
     public function __construct(string $path, string $label)
     {
-        parent::__construct($path);
-
+        $this->path = $path;
         $this->label = $label;
+        $this->converter = [FP::class, 'identity'];
     }
 
     /**
@@ -24,5 +30,40 @@ class LabelledValue extends AbstractLabelledValue
     public function labelMatches(string $label, Context $context): bool
     {
         return $this->label === $label;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @phpstan-param Context<T> $context
+     */
+    public function read($val, Context $context)
+    {
+        return ($this->converter)($val, $context);
+    }
+
+    /**
+     * @phpstan-param Context<T> $context
+     */
+    public function onSectionEnd(Context $context): void
+    {
+    }
+
+    public function getConverter(): callable
+    {
+        return $this->converter;
+    }
+
+    /**
+     * @phpstan-return self<T>
+     */
+    public function setConverter(callable $converter): self
+    {
+        $this->converter = $converter;
+
+        return $this;
     }
 }
