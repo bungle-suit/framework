5ee55b1c55a825b3e8df17e7eef1b5e461254622
Red Forks <redforks@gmail.com>
2021-01-13 15:45:36 +0800

    CSVDecoder

---
+++ src/Encoding/CSV/CSVDecoder.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bungle\Framework\Encoding\CSV;
+
+use Assert\Assertion;
+use Generator;
+
+class CSVDecoder
+{
+    /**
+     * Read csv content from file/stream $f, options:
+     *
+     * charset: convert from charset to utf-8, if specified.
+     * noHeader: treat first row as data, CSVRow indexed as [0..n).
+     *
+     * If contains only head row, returns empty Traversable, to access
+     * the header, use result value ->getReturn() method.
+     *
+     * @param resource $f
+     * @param array{charset?: string, noHeader?: bool} $options
+     * @return Generator<CSVRow>
+     */
+    public static function decode($f, array $options = []): Generator
+    {
+        if ($charset = $options['charset'] ?? false) {
+            stream_filter_append($f, 'convert.iconv.'.$charset.'.utf-8');
+        }
+
+        while (true) {
+            $header = fgetcsv($f);
+            if ($header === false) {
+                return [];
+            }
+
+            Assertion::notNull($header, 'read csv file failed');
+            if ($header[0] === null) {
+                continue;
+            }
+
+            if ($options['noHeader'] ?? false) {
+                $row = $header;
+                $header = range(0, count($row) - 1);
+                yield new CSVRow($header, $row);
+            }
+            break;
+        }
+
+        while (($line = fgetcsv($f)) !== false) {
+            Assertion::notNull($line, 'read csv file failed 2');
+            if ($line[0] === null) {
+                continue;
+            }
+            yield new CSVRow($header, $line);
+        }
+
+        return $header;
+    }
+}
---
+++ tests/Encoding/CSV/CSVDecoderTest.php
@@ -0,0 +1,142 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bungle\Framework\Tests\Encoding\CSV;
+
+use Assert\Assertion;
+use Bungle\Framework\Encoding\CSV\CSVDecoder;
+use Mockery\Adapter\Phpunit\MockeryTestCase;
+
+class CSVDecoderTest extends MockeryTestCase
+{
+    public function testDecodeHasHeader(): void
+    {
+        $f = self::stringStream(
+            <<<'CSV'
+            a,b,c,d
+            1,2,3,4
+            5,6,7,8
+            CSV
+        );
+
+        $gen = CSVDecoder::decode($f);
+        $rowIdx = 0;
+        foreach ($gen as $row) {
+            if ($rowIdx === 0) {
+                self::assertEquals('1', $row['a']);
+                self::assertEquals('4', $row['D']);
+            } elseif ($rowIdx === 1) {
+                self::assertEquals('6', $row['b']);
+                self::assertEquals('7', $row['C']);
+            }
+            $rowIdx++;
+        }
+        self::assertEquals(2, $rowIdx);
+        self::assertEquals(['a', 'b', 'c', 'd'], $gen->getReturn());
+    }
+
+    public function testDecodeNoHeader(): void
+    {
+        $f = self::stringStream(
+            <<<'CSV'
+            1,2,3
+            a,b,c
+            CSV
+        );
+
+        $gen = CSVDecoder::decode($f, ['noHeader' => true]);
+        $rowIdx = 0;
+        foreach ($gen as $row) {
+            if ($rowIdx === 0) {
+                self::assertEquals('1', $row[0]);
+                self::assertEquals('3', $row['C']);
+            } elseif ($rowIdx === 1) {
+                self::assertEquals('b', $row[1]);
+                self::assertEquals('a', $row['A']);
+            }
+            $rowIdx++;
+        }
+        self::assertEquals(2, $rowIdx);
+        self::assertEquals([0, 1, 2], $gen->getReturn());
+    }
+
+    public function testEmpty(): void
+    {
+        $f = self::stringStream('');
+        $gen = CSVDecoder::decode($f);
+        self::assertEquals(0, iterator_count($gen));
+        self::assertEquals([], $gen->getReturn());
+    }
+
+    public function testHasHeaderNoRows(): void
+    {
+        $f = self::stringStream(
+            <<<'CSV'
+            a,b,c,d
+            CSV
+        );
+
+        $gen = CSVDecoder::decode($f);
+        self::assertEquals(0, iterator_count($gen));
+        self::assertEquals(['a', 'b', 'c', 'd'], $gen->getReturn());
+    }
+
+    public function testIgnoreEmptyRows(): void
+    {
+        $f = self::stringStream(
+            <<<'CSV'
+
+            a,b,c,d
+
+            1,2,3,4
+
+            5,6,7,8
+
+
+            CSV
+        );
+
+        $gen = CSVDecoder::decode($f);
+        $rows = iterator_to_array($gen);
+        self::assertSame('1', $rows[0]['A']);
+        self::assertSame('8', $rows[1]['d']);
+        self::assertCount(2, $rows);
+
+        self::assertEquals(['a', 'b', 'c', 'd'], $gen->getReturn());
+    }
+
+    public function testWithCharset(): void
+    {
+        $s = mb_convert_encoding(
+            <<<'CSV'
+            汉字,啊
+            文明,blah
+            CSV
+            ,
+            'GB18030',
+            'utf-8'
+        );
+        Assertion::string($s);
+        $f = self::stringStream($s);
+        $gen = CSVDecoder::decode($f, ['charset' => 'GB18030']);
+        foreach ($gen as $row) {
+            self::assertEquals('文明', $row['A']);
+            self::assertEquals('blah', $row['啊']);
+        }
+        self::assertEquals(['汉字', '啊'], $gen->getReturn());
+    }
+
+    /**
+     * @return resource
+     */
+    private static function stringStream(string $s)
+    {
+        $f = fopen('php://memory', 'r+');
+        self::assertNotFalse($f);
+        fwrite($f, $s);
+        rewind($f);
+
+        return $f;
+    }
+}
