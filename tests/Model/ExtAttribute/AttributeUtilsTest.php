ad9f800c9723cb51466a9f4867eb26708c41186f
Red Forks <redforks@gmail.com>
2021-04-25 20:28:20 +0800

    AttributeUtilsTest.php rewrite to pest

--- tests/Model/ExtAttribute/AttributeUtilsTest.php
+++ tests/Model/ExtAttribute/AttributeUtilsTest.php
@@ -5,31 +5,25 @@ declare(strict_types=1);
 namespace Bungle\Framework\Tests\Model\ExtAttribute;
 
 use Bungle\Framework\Model\ExtAttribute\AttributeUtils;
-use Mockery\Adapter\Phpunit\MockeryTestCase;
 
-class AttributeUtilsTest extends MockeryTestCase
-{
-    public function testGetBoolAttribute(): void
-    {
-        // attribute not exist
-        self::assertFalse(AttributeUtils::getBoolAttribute([], 'foo'));
+it('get bool attribute', function () {
+    // attribute not exist
+    expect(AttributeUtils::getBoolAttribute([], 'foo'))->toBeFalse();
 
-        // attribute exist but false
-        $attr = new TestAttribute('foo', '');
-        self::assertFalse(AttributeUtils::getBoolAttribute([$attr], 'foo'));
+    // attribute exist but false
+    $attr = new TestAttribute('foo', '');
+    expect(AttributeUtils::getBoolAttribute([$attr], 'foo'))->toBeFalse();
 
-        // attribute exist but true
-        $attr->setBool(true);
-        self::assertTrue(AttributeUtils::getBoolAttribute([$attr], 'foo'));
-    }
+    // attribute exist but true
+    $attr->setBool(true);
+    expect(AttributeUtils::getBoolAttribute([$attr], 'foo'))->toBeTrue();
+});
 
-    public function testGetFloatAttribute(): void
-    {
-        // attribute not exist
-        self::assertEquals(0.0, AttributeUtils::getFloatAttribute([], 'foo'));
+it('get float attribute', function () {
+    // attribute not exist
+    expect(AttributeUtils::getFloatAttribute([], 'foo'))->toBe(0.0);
 
-        // attribute exist but false
-        $attr = new TestAttribute('foo', '123.45');
-        self::assertEquals(123.45, AttributeUtils::getFloatAttribute([$attr], 'foo'));
-    }
-}
+    // attribute exist but false
+    $attr = new TestAttribute('foo', '123.45');
+    expect(AttributeUtils::getFloatAttribute([$attr], 'foo'))->toBe(123.45);
+});
