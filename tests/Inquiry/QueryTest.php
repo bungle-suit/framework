a809ceabff3f23485cc64b580e0d8845f6e28a15
Red Forks <redforks@gmail.com>
2021-06-02 16:23:44 +0800

    QueryTest.php rewrite to pest

--- tests/Inquiry/QueryTest.php
+++ tests/Inquiry/QueryTest.php
@@ -20,150 +20,136 @@ use Mockery\Adapter\Phpunit\MockeryTestCase;
 use Symfony\Component\PropertyInfo\Type;
 use Traversable;
 
-class QueryTest extends MockeryTestCase
+beforeEach(function ()
 {
-    /** @var EntityManagerInterface|Mockery\MockInterface */
-    private $em;
+    $this->em = Mockery::mock(EntityManagerInterface::class);
+});
 
-    protected function setUp(): void
-    {
-        parent::setUp();
+it('query', function() {
+    $qb = Mockery::mock(QueryBuilder::class);
+    $this->em->expects('createQueryBuilder')
+             ->andReturn($qb);
+    $dqlQuery = Mockery::mock(AbstractQuery::class);
+    $qb->expects('getQuery')
+       ->andReturn($dqlQuery);
+    $dqlQuery->expects('iterate')
+             ->with(null, AbstractQuery::HYDRATE_ARRAY)
+             ->andReturn(
+                 new ArrayIterator(
+                     [
+                         new ArrayIterator([['line1'], ['line2']]),
+                         new ArrayIterator([['line3']]),
+                         new ArrayIterator([]),
+                     ]
+                 ),
+             );
 
-        $this->em = Mockery::mock(EntityManagerInterface::class);
-    }
+    $params = new QueryParams(0, []);
+    $step1 = Mockery::mock(QueryStepInterface::class);
+    $step2 = Mockery::mock(QueryStepInterface::class);
+    $step1->expects('__invoke')
+          ->with(
+              Mockery::on(
+                  fn(Builder $builder) => $builder->getQueryParams() === $params &&
+                      $builder->getQueryBuilder() === $qb
+              )
+          );
+    $step2->expects('__invoke')
+          ->with(Mockery::type(Builder::class));
+    $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
+    $q1 = new QBEMeta('fooMeta', 'lbl', new Type(Type::BUILTIN_TYPE_INT, true));
+    $q = new Query(
+        $this->em,
+        [
+            $step1,
+            $step2,
+            function (Builder $builder) use ($q1, $col1): void {
+                $builder->addColumn($col1, 'foo');
+                $builder->addQBE($q1);
+            },
+        ]
+    );
 
-    public function testQuery(): void
-    {
-        $qb = Mockery::mock(QueryBuilder::class);
-        $this->em->expects('createQueryBuilder')
-                 ->andReturn($qb);
-        $dqlQuery = Mockery::mock(AbstractQuery::class);
-        $qb->expects('getQuery')
-           ->andReturn($dqlQuery);
-        $dqlQuery->expects('iterate')
-                 ->with(null, AbstractQuery::HYDRATE_ARRAY)
-                 ->andReturn(
-                     new ArrayIterator(
-                         [
-                             new ArrayIterator([['line1'], ['line2']]),
-                             new ArrayIterator([['line3']]),
-                             new ArrayIterator([]),
-                         ]
-                     ),
-                 );
+    $iter = $q->query($params);
+    expect($q->getColumns())->toEqual(['foo' => $col1]);
+    expect(iterator_to_array($iter, false))->toEqual([['line1'], ['line2'], ['line3']]);
+});
 
-        $params = new QueryParams(0, []);
-        $step1 = Mockery::mock(QueryStepInterface::class);
-        $step2 = Mockery::mock(QueryStepInterface::class);
-        $step1->expects('__invoke')
-              ->with(
-                  Mockery::on(
-                      fn(Builder $builder) => $builder->getQueryParams() === $params &&
-                          $builder->getQueryBuilder() === $qb
-                  )
-              );
-        $step2->expects('__invoke')
-              ->with(Mockery::type(Builder::class));
-        $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
-        $q1 = new QBEMeta('fooMeta', 'lbl', new Type(Type::BUILTIN_TYPE_INT, true));
-        $q = new Query(
-            $this->em,
-            [
-                $step1,
-                $step2,
-                function (Builder $builder) use ($q1, $col1): void {
-                    $builder->addColumn($col1, 'foo');
-                    $builder->addQBE($q1);
-                },
-            ]
-        );
+it('paged query', function() {
+    $paginator = Mockery::mock('overload:\Doctrine\ORM\Tools\Pagination\Paginator', Traversable::class, Countable::class);
+    $paginator->expects('count')->andReturn(11);
+    $paginator->expects('setUseOutputWalkers')->with(false);
+    $paginator->expects('getIterator')->with()->andReturn(
+        new ArrayIterator([
+                              ['line1'],
+                              ['line2'],
+                          ])
+    );
 
-        $iter = $q->query($params);
-        self::assertEquals(['foo' => $col1], $q->getColumns());
-        self::assertEquals(
-            [['line1'], ['line2'], ['line3']],
-            iterator_to_array($iter, false)
-        );
-    }
+    $qb = Mockery::mock(QueryBuilder::class);
+    $this->em->expects('createQueryBuilder')
+             ->andReturn($qb);
+    $dqlQuery = Mockery::mock(AbstractQuery::class);
+    $qb->expects('getQuery')
+       ->andReturn($dqlQuery);
 
-    public function testPagedQuery(): void
-    {
-        $paginator = Mockery::mock('overload:\Doctrine\ORM\Tools\Pagination\Paginator', Traversable::class, Countable::class);
-        $paginator->expects('count')->andReturn(11);
-        $paginator->expects('setUseOutputWalkers')->with(false);
-        $paginator->expects('getIterator')->with()->andReturn(
-            new ArrayIterator([
-                                  ['line1'],
-                                  ['line2'],
-                              ])
-        );
+    $params = new QueryParams(0, []);
+    $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
+    $pagingStep = Mockery::namedMock('pagingStep', QueryStepInterface::class);
+    $q = new class(
+        $this->em,
+        [
+            function (Builder $builder) use ($col1): void {
+                $builder->addColumn($col1, 'foo');
+            },
+        ],
+        $pagingStep,
+    ) extends Query {
+        private QueryStepInterface $pagingStep;
 
-        $qb = Mockery::mock(QueryBuilder::class);
-        $this->em->expects('createQueryBuilder')
-                 ->andReturn($qb);
-        $dqlQuery = Mockery::mock(AbstractQuery::class);
-        $qb->expects('getQuery')
-           ->andReturn($dqlQuery);
+        public function __construct(
+            EntityManagerInterface $em,
+            array $steps,
+            QueryStepInterface $pagingStep
+        ) {
+            parent::__construct($em, $steps);
+            $this->pagingStep = $pagingStep;
+        }
 
-        $params = new QueryParams(0, []);
-        $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
-        $pagingStep = Mockery::namedMock('pagingStep', QueryStepInterface::class);
-        $q = new class(
-            $this->em,
-            [
-                function (Builder $builder) use ($col1): void {
-                    $builder->addColumn($col1, 'foo');
-                },
-            ],
-            $pagingStep,
-        ) extends Query {
-            private QueryStepInterface $pagingStep;
+        protected function createExtraPagingSteps(): array
+        {
+            return [$this->pagingStep];
+        }
+    };
+    $pagingStep->expects('__invoke')
+               ->with(Mockery::on(fn(Builder $builder) => count($builder->getColumns()) === 1));
 
-            public function __construct(
-                EntityManagerInterface $em,
-                array $steps,
-                QueryStepInterface $pagingStep
-            ) {
-                parent::__construct($em, $steps);
-                $this->pagingStep = $pagingStep;
-            }
+    $pagedData = $q->pagedQuery($params);
+    expect($pagedData->getCount())->toEqual(11);
+    expect($pagedData->getData())->toEqual([['line1'], ['line2']]);
+    expect($q->getColumns())->toEqual(['foo' => $col1]);
+});
 
-            protected function createExtraPagingSteps(): array
-            {
-                return [$this->pagingStep];
-            }
-        };
-        $pagingStep->expects('__invoke')
-                   ->with(Mockery::on(fn(Builder $builder) => count($builder->getColumns()) === 1));
+it('build QBE Meta', function() {
+    $qb = Mockery::mock(QueryBuilder::class);
+    $this->em->expects('createQueryBuilder')
+             ->andReturn($qb);
 
-        $pagedData = $q->pagedQuery($params);
-        self::assertEquals(11, $pagedData->getCount());
-        self::assertEquals([['line1'], ['line2']], $pagedData->getData());
-        self::assertEquals(['foo' => $col1], $q->getColumns());
-    }
+    $params = new QueryParams(0, []);
+    $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
+    $q1 = new QBEMeta('fooMeta', 'lbl', new Type(Type::BUILTIN_TYPE_INT, true));
+    $q = new Query(
+        $this->em,
+        [
+            function (Builder $builder) use ($q1, $col1): void {
+                self::assertTrue($builder->isBuildForQBE());
+                $builder->addColumn($col1, 'foo');
+                $builder->addQBE($q1);
+            },
+        ]
+    );
 
-    public function testBuildQBEMetas(): void
-    {
-        $qb = Mockery::mock(QueryBuilder::class);
-        $this->em->expects('createQueryBuilder')
-                 ->andReturn($qb);
-
-        $params = new QueryParams(0, []);
-        $col1 = new ColumnMeta('[id]', 'id', new Type(Type::BUILTIN_TYPE_INT));
-        $q1 = new QBEMeta('fooMeta', 'lbl', new Type(Type::BUILTIN_TYPE_INT, true));
-        $q = new Query(
-            $this->em,
-            [
-                function (Builder $builder) use ($q1, $col1): void {
-                    self::assertTrue($builder->isBuildForQBE());
-                    $builder->addColumn($col1, 'foo');
-                    $builder->addQBE($q1);
-                },
-            ]
-        );
-
-        $QBEs = $q->buildQBEMetas($params);
-        self::assertEquals(['fooMeta' => $q1], $q->getQBEMetas());
-        self::assertSame($QBEs, $q->getQBEMetas());
-    }
-}
+    $QBEs = $q->buildQBEMetas($params);
+    expect($q->getQBEMetas())->toEqual(['fooMeta' => $q1]);
+    expect($q->getQBEMetas())->toBe($QBEs);
+});
