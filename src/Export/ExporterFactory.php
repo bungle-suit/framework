a65611ee9d1bf62266dde194dca345b68575c34a
forks <redforks@gmail.com>
2021-11-19 19:54:15 +0800

    Expose ExporterFactory service

--- src/Export/ExporterFactory.php
+++ src/Export/ExporterFactory.php
@@ -4,22 +4,20 @@ declare(strict_types=1);
 
 namespace Bungle\Framework\Export;
 
-use Symfony\Component\DependencyInjection\Attribute\TaggedLocator;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
 class ExporterFactory
 {
     public const SERVICE_TAG = 'bungle.exporter';
 
-    private ContainerInterface $container;
-
-    public function __construct(#[TaggedLocator(self::SERVICE_TAG)] ContainerInterface $container)
-    {
-        $this->container = $container;
-    }
+    /** @var AbstractExporter[] */
+    private array $exporters = [];
 
     public function getExporter(string $exporterClass): AbstractExporter
     {
-        return $this->container->get($exporterClass);
+        return $this->exporters[$exporterClass];
+    }
+
+    public function addExporter(AbstractExporter $exporter): void
+    {
+        $this->exporters[$exporter::class] = $exporter;
     }
 }
