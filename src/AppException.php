d6869ee890eff69b20885463b9db7dd098faad38
forks <redforks@gmail.com>
2022-07-08 17:44:16 +0800

    AppException

---
+++ src/AppException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bungle\Framework;
+
+use Exception;
+
+/**
+ * Application Exception (User Exception), error caused by
+ * Application domain
+ */
+class AppException extends Exception
+{
+}
