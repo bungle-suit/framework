c9b8dc1d2bc25ba8d7ba52e3d25f1765c4685052
forks <redforks@gmail.com>
2022-09-24 16:49:29 +0800

    Filters::afterThatTime()

--- src/Filters.php
+++ src/Filters.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace Bungle\Framework;
 
+use Bungle\Framework\Ent\BasalInfoService;
+use DateTime;
+
 class Filters
 {
+    public function __construct(private BasalInfoService $basal)
+    {
+    }
+
     /**
      * @template T
      * @param class-string<T> $type
@@ -15,6 +22,16 @@ class Filters
     {
         return function ($value) use ($type) {
             return $value instanceof $type;
+        };
+    }
+
+    /**
+     * @return callable(): bool Return true if now equal or after $time
+     */
+    public function afterThatTime(DateTime $time): callable
+    {
+        return function () use ($time) {
+            return $this->basal->now() >= $time;
         };
     }
 }
