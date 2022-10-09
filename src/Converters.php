b2f727fcbe00f8aa89807292484abc4fccfd3175
forks <redforks@gmail.com>
2022-10-09 11:39:14 +0800

    Converters::assocArrayFrom() prop can be [path, callback]

--- src/Converters.php
+++ src/Converters.php
@@ -16,7 +16,9 @@ class Converters
      * Create assoc array from string or callback, use PropertyAccessor to
      * get value from $o.
      * @template T
-     * @param array<string, string|callable(T): mixed> $props
+     * @param array<string, string|callable|array{string, callable}> $props ,
+     *  can be prop path, callable($o) or [prop path, callable($propValue, $o)],
+     * in third case, callable convert value get from path.
      * @return callable(T): array<string, mixed>
      */
     public function assocArrayFrom(array $props): callable
@@ -24,7 +26,13 @@ class Converters
         return function ($o) use ($props): array {
             $ret = [];
             foreach ($props as $k => $v) {
-                $ret[$k] = is_callable($v) ? $v($o) : $this->propAcc->getValue($o, $v);
+                if (is_callable($v)) {
+                    $ret[$k] = $v($o);
+                } elseif (is_array($v)) {
+                    $ret[$k] = $v[1]($this->propAcc->getValue($o, $v[0]), $o);
+                } else {
+                    $ret[$k] = $this->propAcc->getValue($o, $v);
+                }
             }
 
             return $ret;
